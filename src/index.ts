69c0d530a85cb4be59356f49eb18270b0850b567
Henrik Wenz <henrik.wenz@gmail.com>
2026-07-01 14:33:18 +0200

    chore: Add dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export function fn() {
-  return 'Hello, tsdown!'
-}
+import type { Plugin } from "eslint";
+
+const plugin: Plugin = {
+  meta: {},
+  configs: {},
+  rules: {},
+  processors: {},
+};
+
+export default plugin;
