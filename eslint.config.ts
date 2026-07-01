7d93959e8327febfd38f22755317308844a2524c
Henrik Wenz <henrik.wenz@gmail.com>
2026-07-01 21:24:04 +0200

    fix: linter

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,10 @@
-import eslintPlugin from "eslint-plugin-eslint-plugin";
 import { defineConfig } from "eslint/config";
+import eslint from "eslint-plugin-eslint-plugin";
+import js from "@eslint/js";
+import ts from "typescript-eslint";
 
-export default defineConfig([eslintPlugin.configs.recommended]);
+export default defineConfig([
+  eslint.configs.recommended,
+  js.configs.recommended,
+  ts.configs.recommended,
+]);
