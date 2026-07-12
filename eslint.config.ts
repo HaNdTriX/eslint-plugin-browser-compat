9f0ee808707b491c407b7120ed4de4666253f659
Henrik Wenz <henrik.wenz@gmail.com>
2026-07-12 18:02:46 +0200

    fix: eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import eslint from "eslint-plugin-eslint-plugin";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
 
 export default defineConfig([
+  globalIgnores([
+    "**/node_modules/**",
+    "**/dist/**",
+    "**/build/**",
+    "**/coverage/**",
+  ]),
   eslint.configs.recommended,
   js.configs.recommended,
   ts.configs.recommended,
